ebea416c49de6eaea0219caff331cb67fcd4cba8
Arnav Gupta <arnav@codingblocks.com>
2016-12-29 19:02:02 +0530

    fix regex'
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -10,7 +10,7 @@ const SHORTENER_SECRET = process.env.SHORTURL_SECRET || "cb@123";
 
 route.post('/shorten', function (req, res) {
     let url = req.body.url;
-    var http = /^http?(s):\/\//i;
+    var http = /^https?:\/\//i;
     if(http.test(url) == false)
         url = "http://"+url;
     let secret = req.body.secret;

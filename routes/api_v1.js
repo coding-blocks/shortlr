fbd4254499f628f3e318346ac1e8e878100fef87
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-08-15 01:46:56 +0530

    Search by LongURL
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- routes/api_v1.js
+++ routes/api_v1.js
@@ -1,4 +1,3 @@
-
 /**
  * Created by championswimmer on 14/12/16.
  */
@@ -8,80 +7,105 @@ const route = express.Router();
 
 const shortner = require('../utils/shortner');
 const SHORTENER_SECRET = process.env.SHORTURL_SECRET || "cb@123";
-
+const SHORTENER_LONGURL_SECRET = process.env.SHORTURL_LONGURL_SECRET || "cb@123";
+const db = require('./../utils/db');
 
 route.post('/shorten', function (req, res) {
-    let url = req.body.url;
-    var http = /^https?:\/\//i;
-    if(http.test(url) == false)
-        url = "http://"+url;
-    let secret = req.body.secret;
-    let code = null;
-    if (secret == SHORTENER_SECRET) {
-        console.log('Using secret');
-        code = req.body.code;
-        if (code && code.length > 9) {
-            console.log('Too long code');
-            return res.send("We do not support larger than 9 character")
-        }
+  let url = req.body.url;
+  var http = /^https?:\/\//i;
+  if (http.test(url) == false)
+    url = "http://" + url;
+  let secret = req.body.secret;
+  let code = null;
+  if (secret == SHORTENER_SECRET) {
+    console.log('Using secret');
+    code = req.body.code;
+    if (code && code.length > 9) {
+      console.log('Too long code');
+      return res.send("We do not support larger than 9 character")
     }
+  }
 
-    shortner.shorten(url, code, function (shortcode, existed, longURL) {
-        res.send({
-            shortcode, existed, longURL
-        });
+  shortner.shorten(url, code, function (shortcode, existed, longURL) {
+    res.send({
+      shortcode, existed, longURL
     });
+  });
 });
 
 route.get('/expand/:shortcode', function (req, res) {
-    if(req.params.shortcode.length > 9) {
-        res.redirect(200, 'http://www.codingblocks.com');
-    }
+  if (req.params.shortcode.length > 9) {
+    res.redirect(200, 'http://www.codingblocks.com');
+  }
 
-    if (!req.params.shortcode || req.params.shortcode.length == 0) {
+  if (!req.params.shortcode || req.params.shortcode.length == 0) {
+    res.send({
+      status: 501,
+      message: "Wrong shortcode, or no shortcode given"
+    })
+  } else {
+    shortner.expand(req.params.shortcode, req.headers.referer, function (URL) {
+      if (URL) {
         res.send({
-            status: 501,
-            message: "Wrong shortcode, or no shortcode given"
+          status: 200,
+          url: URL
         })
+      } else {
+        res.send({
+          status: 501,
+          message: "Server error"
+        })
+      }
+    });
+  }
+
+});
+
+route.post('/longURL/expand', function (req, res) {
+  if (req.body.secret !== SHORTENER_LONGURL_SECRET) {
+    return res.send({
+      status: 401,
+      message: "Wrong secret Code"
+    })
+  }
+  console.log(req.query.longcode);
+  db.fetchLongUrl(req.query.longcode, function (urls) {
+    if (urls.length === 0) {
+      return res.send({
+        status: 404,
+        message: 'No Shortlinks found for this URL'
+      })
     } else {
-        shortner.expand(req.params.shortcode, req.headers.referer, function (URL) {
-            if (URL) {
-                res.send({
-                    status: 200,
-                    url: URL
-                })
-            } else {
-                res.send({
-                    status: 501,
-                    message: "Server error"
-                })
-            }
-        });
+      return res.send({
+        status: 200,
+        urls: urls
+      })
     }
+  })
 
 });
 
 route.get('/stats', function (req, res) {
-    const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl.split("?").shift() ;
+  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl.split("?").shift();
 
-    let {page,size} = req.query ;
-    page = parseInt(page) || 1;
-    size = parseInt(size) || config.PAGINATION_SIZE;
+  let {page, size} = req.query;
+  page = parseInt(page) || 1;
+  size = parseInt(size) || config.PAGINATION_SIZE;
 
-    shortner.stats(page,size,fullUrl).then( ({urls,prevPage,nextPage,lastPage})=>{
-        const meta = { prevPage,nextPage,lastPage };
-        res.json({urls,meta});
-    }).catch(err=>{
-        console.error(err);
-        res.send ({
-            code: 501,
-            message: "Error occured"
-        })
-    });
+  shortner.stats(page, size, fullUrl).then(({urls, prevPage, nextPage, lastPage}) => {
+    const meta = {prevPage, nextPage, lastPage};
+    res.json({urls, meta});
+  }).catch(err => {
+    console.error(err);
+    res.send({
+      code: 501,
+      message: "Error occured"
+    })
+  });
 });
 
 route.use((req, res) => {
-    res.send("This is not the way to use the api")
+  res.send("This is not the way to use the api")
 });
 
 module.exports = route;

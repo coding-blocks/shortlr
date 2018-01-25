bec4b75e5ff0759dfa36063f4bdda5d32e5310de
Arnav Gupta <arnav@codingblocks.com>
2018-01-25 21:07:34 +0530

    if too long, send to home
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- routes/shortcode.js
+++ routes/shortcode.js
@@ -1,63 +1,67 @@
 /**
  * Created by championswimmer on 14/12/16.
  */
-const express = require('express');
-const route = express.Router();
+const express = require('express')
+const route = express.Router()
 
-const models = require('./../utils/db').models;
+const models = require('./../utils/db').models
 
-const shortner = require('../utils/shortner');
+const shortner = require('../utils/shortner')
 
 route.get('/:groupName/:shortcode', (req, res, next) => {
-  let code = req.params.shortcode;
-  if (!req.params.shortcode || req.params.shortcode.length === 0) {
-    next();
-  }
-  const groupName = req.params.groupName;
-  let tempCode = req.params.shortcode;
-
-  models.Group.findOne({
-    where: {groupName: groupName},
-  }).then(function (group) {
-    if (group) {
-      tempCode = "" + group.id + tempCode;
-      while (tempCode.length < 9) {
-        tempCode = tempCode + "0";
-      }
-      code = tempCode;
-      shortner.expand(code, req.headers.referer, function (URL) {
-        if (!URL) {
-          next();
-        } else {
-          res.redirect(URL);
-        }
-      });
-    } else {
-      next();
+    let code = req.params.shortcode
+    if (!req.params.shortcode || req.params.shortcode.length === 0) {
+        next()
     }
-  }).catch(function (err) {
-    console.log(err);
-    return res.send("Internal Server Error");
-  });
+    const groupName = req.params.groupName
+    let tempCode = req.params.shortcode
 
-});
+    models.Group.findOne({
+        where: {groupName: groupName},
+    }).then(function (group) {
+        if (group) {
+            tempCode = "" + group.id + tempCode
+            while (tempCode.length < 9) {
+                tempCode = tempCode + "0"
+            }
+            code = tempCode
+            shortner.expand(code, req.headers.referer, function (URL) {
+                if (!URL) {
+                    next()
+                } else {
+                    res.redirect(URL)
+                }
+            })
+        } else {
+            next()
+        }
+    }).catch(function (err) {
+        console.log(err)
+        return res.send("Internal Server Error")
+    })
+
+})
 
 route.get('/:shortcode', (req, res, next) => {
 
-  if (!req.params.shortcode || req.params.shortcode.length == 0) {
-    return next();
-  }
-
-  req.params.shortcode = req.params.shortcode.trim()
-
-  shortner.expand(req.params.shortcode, req.headers.referer, function (URL) {
-    if (!URL) {
-      next();
-    } else {
-      res.redirect(URL);
+    if (!req.params.shortcode || req.params.shortcode.length === 0) {
+        return next()
     }
-  });
 
-});
+    req.params.shortcode = req.params.shortcode.trim()
 
-module.exports = route;
+    if (req.params.shortcode.length > 9) {
+        return next()
+    }
+
+    shortner.expand(req.params.shortcode, req.headers.referer, function (URL) {
+        if (!URL) {
+            next()
+        } else {
+            res.redirect(URL)
+        }
+    })
+
+})
+
+module.exports = route
